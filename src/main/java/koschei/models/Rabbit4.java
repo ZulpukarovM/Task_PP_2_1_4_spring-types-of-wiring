68628dbafeda99493a117fe5ea384fb2581b9845
Magomed Zulpukarov <zulpukarovmagomed00@gmail.com>
2024-09-11 20:15:07 +0300

    Переделал

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Rabbit4 {
 
-    private Duck5 duck = new Duck5();
+    private Duck5 duck;
 
     @Autowired
     public void setDuck(Duck5 duck) {
